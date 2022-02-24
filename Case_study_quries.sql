343f9386756f1c4a6b327f63c9774ce6bda4f02f
apraturi-FT <100192167+apraturi-FT@users.noreply.github.com>
2022-02-24 14:37:03 +0530

    New changes enter

--- Case_study_quries.sql
+++ Case_study_quries.sql
@@ -1,4 +1,5 @@
 1)
+
 select customer_id,sum(price) total_amount_spent from(select sales.customer_id,sales.product_id,menu.price from sales inner join menu on sales.product_id=menu.product_id) group by customer_id;
 
 
